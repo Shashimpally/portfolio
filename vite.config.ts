5539c6098f5ed40e3ef2fe805323e66ed00531f5
Mulakalapally Shashikumar <shashimpally02@gmail.com>
2025-07-05 20:00:36 +0530

    files changed

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-// vite.config.js
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/portfolio/', // 👈 this should match your GitHub repo name
   plugins: [react()],
 })
